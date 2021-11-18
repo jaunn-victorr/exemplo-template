185fe4c456a6ab7f748054185aff2c2a7ea340f7
jaunn-victorr <joao.vitor.ptal@gmail.com>
2021-11-18 17:48:26 +0000

    mudancas layout

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [nome,setNome] =useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Primeiro Aplicativo</Text>
+      <TextInput style={styles.campo} value={nome} placeholder="digite seu nome" onChangeText={(nome)=> SetNome(nome)}>
+      </TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +21,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titulo:{
+    fontFamily: 'prestige elite sd',
+    fontSize: 30,
+    textAlign:'center',
+    marginTop: 25 
+  },
+  campo:{
+    backgroundColor:'#121212',
+    borderRadius: 10,
+    fontSize: 23,
+    color:'#FFF',
+    margin: 10,
+    padding: 10
+  },
+
+botao:{
+  backgroundColor:'#41AEF4',
+  alignItems:'center',
+  justifyContent:'center',
+  margin: 15,
+  padding: 10
+},
+
+textoBotao:{
+  fontSize: 25,
+  color: '#FFF'
+},
 });
